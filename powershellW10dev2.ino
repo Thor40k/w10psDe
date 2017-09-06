14bc9afb456063d0b31c1c2645af3a8c96b9f2d3
Thor40k <the.thor@gmail.com>
2017-09-06 10:57:56 +0200

    Update powershellW10dev2.ino

--- powershellW10dev2.ino
+++ powershellW10dev2.ino
@@ -18,22 +18,17 @@ void setup(){
   
   DigiKeyboard.update();
   if (iterationCounter == 0) {
-    // this is generally not necessary but with some older systems it seems to
-    // prevent missing the first character after a delay:
     DigiKeyboard.sendKeyStroke(0);
 
-    // It's better to use DigiKeyboard.delay() over the regular Arduino delay()
-    // if doing keyboard stuff because it keeps talking to the computer to make
-    // sure the computer knows the keyboard is alive and connected
     DigiKeyboard.delay(KEYSTROKE_DELAY);
 
   DigiKeyboard.delay(6000);
-  DigiKeyboard.sendKeyStroke(21,128); //R + Windows Keys
+  DigiKeyboard.sendKeyStroke(21,128); //R + Windows Taste
   DigiKeyboard.delay(2000);
-  DigiKeyboard.print("powershell Start/Process cmd /Verb runAs");
+  DigiKeyboard.print("powershell Start/Process cmd /Verb runAs"); //Powershell als Admin
   DigiKeyboard.sendKeyStroke(KEY_ENTER);
   DigiKeyboard.delay(2000);
-  DigiKeyboard.sendKeyStroke(KEY_Z, MOD_ALT_LEFT); // ALT-J 
+  DigiKeyboard.sendKeyStroke(KEY_Z, MOD_ALT_LEFT); // ALT-J für System deutsches KB, US Sparche
   //DigiKeyboard.sendKeyStroke(28,64); //Y + right ALT
   DigiKeyboard.delay(2000);
   DigiKeyboard.print("mode con>cols)18 lines)1");
